d602775def5514eccdc97d385319712cffe69d85
kachenieks <katrinablazkova06@gmail.com>
2025-10-28 09:17:02 +0200

    Android changes 28.10.2025

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,83 +1,172 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class CameraScript : MonoBehaviour 
+// CHANGES FOR ANDROID
+public class CameraScript : MonoBehaviour
 {
     public float maxZoom = 530f;
-    public float minZoom = 1f;
+    public float minZoom = 150f;
     public float panSpeed = 6f;
-    
-    Vector3 bottomLeft, topRight;
-    float cameraMaxX, cameraMinX, cameraMaxY, cameraMinY, x, y;
+    float startZoom;
+    public float pinchZoomSpeed = 0.9f, mouseZoomSpeed = 150f;
+    public float mouseFollowSpeed = 1f, touchPanSpeed = 1f;
+    public ScreenBoundriesScript screenBoundries;
     float initialZoom;
     public Camera cam;
+    Vector2 lasTouchPos;
+    int panFingerId = -1;
+    bool isTouchPaning = false;
+    float lasTapTime = 0f;
+    public float doubleTapMaxDelay = 0.4f;
+    public float doubleTapMaxDistance = 100f;
 
-    void Start() 
+    void Awake()
     {
-        cam = GetComponent<Camera>();
-        initialZoom = cam.orthographicSize;
-        topRight = cam.ScreenToWorldPoint(
-            new Vector3(cam.pixelWidth, cam.pixelHeight, -transform.position.z));
-        bottomLeft = cam.ScreenToWorldPoint(
-            new Vector3(0, 0, -transform.position.z));
-        cameraMaxX = topRight.x;
-        cameraMinX = bottomLeft.x;
-        cameraMaxY = topRight.y;
-        cameraMinY = bottomLeft.y;
+        if (cam == null)
+        {
+            cam = GetComponent<Camera>();
+        }
+        if(screenBoundries == null)
+        {
+            screenBoundries = FindFirstObjectByType<ScreenBoundriesScript>();
+        }
     }
 
-    void Update() 
+    void Start()
     {
-        x = Input.GetAxis("Mouse X") * panSpeed;
-        y = Input.GetAxis("Mouse Y") * panSpeed;
-        transform.Translate(x, y, 0);
+        startZoom = cam.orthographicSize;
+        screenBoundries.RecalculateBounds();
+        transform.position = ScreenBoundries.GetClampedPosition(transform.position);
+    }
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+    void Update()
+    {
+        if(TransformationScript.isTransforming)
+            return;
+
+        #if UNITY_EDITOR || UNITY_STANDALONE
+        DesktopFollowCursor();
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if(Mathf.Abs(scroll) > Mathf.Epsilon){
+            cam.orthographicSize -= scroll * mouseZoomSpeed;
+        }
+        #else
+        HandleTouch();
+        #endif
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minZoom, maxZoom);
+        screenBoundries.RecalculateBounds();
+        transform.position = screenBoundries.GetClampedPosition(transform.position);
+    }
+
+    void DesktopFollowCursor()
+    {
+        Vector3 mouse = Input.mousePosition;
+        if (mouse.x < 0 || mouse.x > Screen.width || mouse.x < 0 || mouse.y > Screen.width)
+            return;
+
+        Vector3 screenPoint = new Vector3(mouse.x, mouse.y, cam.nearClipPlane);
+        Vector3 targetWorld = cam.ScreenToWorldPoint(screenPoint);
+        Vector3 desired = new Vector3(targetWorld.x, targetWorld.y, transform.position.z);
+
+        transform.position =
+            Vector3.Lerp(transform.position, desired, Time.deltaTime * mouseFollowSpeed);
+    }
+
+    void HandleTouch()
+    {
+        if (Input.touchCount == -1)
+            return;
+
+        Touch t = Input.GetTouch(0);
+
+        if (isTouchingOverUIButton(t.position))
+            return;
+
+        if (t.phase == TouchPhase.Began)
         {
-            if (cam.orthographicSize > minZoom)
+            float dt = Time.time - lasTapTime;
+            if (dt <= doubleTapMaxDelay &&
+                Vector2.Distance(t.position, lasTouchPos) <= doubleTapMaxDistance)
             {
-                cam.orthographicSize = cam.orthographicSize - 50f;
+                StartCoroutine(ResetZoomSmooth());
+                lasTapTime = 0f;
             }
-        }
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
-        {
-            if (cam.orthographicSize < maxZoom)
+            else
             {
-                cam.orthographicSize = cam.orthographicSize + 50f;
+                lasTapTime = Time.time;
             }
-        }
+            lasTouchPos = t.position;
+            panFingerId = t.fingerId;
+            isTouchPaning = true;
 
-        topRight = cam.ScreenToWorldPoint(
-            new Vector3(cam.pixelWidth, cam.pixelHeight, -transform.position.z));
-        bottomLeft = cam.ScreenToWorldPoint(
-            new Vector3(0, 0, -transform.position.z));
+        }
+        else if (t.phase == TouchPhase.Moved && isTouchPaning && t.fingerId == panFingerId)
+        {
+            Vector2 delta = t.position - lasTouchPos;
+            transform.Translate(ScreenDeltaToWorldDelta(delta) * touchPanSpeed, Space.World);
+            lasTouchPos = t.position;
 
-        if (topRight.x > cameraMaxX)
-        {
-            transform.position = new Vector3(
-                transform.position.x - (topRight.x - cameraMaxX),
-                transform.position.y,
-                transform.position.z);
         }
-        if (topRight.y > cameraMaxY)
+        else if (t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled)
         {
-            transform.position = new Vector3(
-                transform.position.x,
-                transform.position.y - (topRight.y - cameraMaxY),
-                transform.position.z);
-        }
-        if (bottomLeft.x < cameraMinX)
-        {
-            transform.position = new Vector3(
-                transform.position.x + (cameraMinX - bottomLeft.x),
-                transform.position.y,
-                transform.position.z);
-        }
-        if (bottomLeft.y < cameraMinY)
-        {
-            transform.position = new Vector3(
-                transform.position.x,
-                transform.position.y + (cameraMinY - bottomLeft.y),
-                transform.position.z);
+            isTouchPaning = false;
+            panFingerId = -1;
         }
     }
+
+    bool isTouchingOverUIButton(Vector2 touchPos)
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+        pointerData.position = touchPos;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.GetComponent<UnityEngine.UI.Button>() != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void HandlePinch()
+    {
+        Touch t0 = Input.GetTouch(0);
+        Touch t1 = Input.GetTouch(1);
+
+        float prevDist = (t0.position - t0.deltaPosition - (t1.position - t1.deltaPosition)).magnitude;
+        float currDist = (t0.position - t1.position).magnitude;
+        cam.orthographicSize -= (prevDist - currDist) * pinchZoomSpeed;
+    }
+
+    Vector3 ScreenDeltaToWorldDelta(Vector2 screenDelta)
+    {
+        float worldPerPixel = (2f * cam.orthographicSize) / Screen.height;
+        return new Vector3(screenDelta.x * worldPerPixel, screenDelta.y * worldPerPixel, 0f);
+    }
+
+    IEnumerator ResetZoomSmooth()
+    {
+        float duration = 0.25f;
+        float elapsed = 0f;
+        float initialZoom = cam.orthographicSize;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            cam.orthographicSize = Mathf.Lerp(initialZoom, startZoom, elapsed / duration);
+            screenBoundries.RecalculateBounds();
+            transform.position = screenBoundries.GetClampedPosition(transform.position);
+            yield return null;
+        }
+        cam.orthographicSize = startZoom;
+        screenBoundries.RecalculateBounds();
+        transform.position = screenBoundries.GetClampedPosition(transform.position);
+    }
+    
+
 }
--- Assets/Scripts/TransformationScript.cs
+++ Assets/Scripts/TransformationScript.cs
@@ -2,51 +2,63 @@ using UnityEngine;
 
 public class TransformationScript : MonoBehaviour
 {
-    [Header("Izmēra ierobežojumi")]
-    public float minScale = 0.3f;
-    public float maxScale = 1.05f;
-    public float scaleSpeed = 0.005f;
+    public float rotationSpeed = 90f;
+    public float scaleSpeed = 0.5f;
 
-    [Header("Rotācijas ātrums")]
-    public float rotationSpeed = 15f;
+    private bool rotateCW, rotateCCW, scaleUpY, scaleDownY, scaleUpX, scaleDownX;
+    public static bool isTransforming = false;
+    // [Header("Izmēra ierobežojumi")]
+    // public float minScale = 0.3f;
+    // public float maxScale = 1.05f;
+    // public float scaleSpeed = 0.005f;
+
+    // [Header("Rotācijas ātrums")]
+    // public float rotationSpeed = 15f;
 
     void Update()
     {
-        if (ObjectScript.lastDragged == null) return;
+        if (ObjectScript.lastDragged == null)
+            return;
 
-        RectTransform rect = ObjectScript.lastDragged.GetComponent<RectTransform>();
-        Vector3 currentScale = rect.localScale;
+        RectTransform rt = ObjectScript.lastDragged.GetComponent<RectTransform>();
 
-        // ===== ROTĀCIJA =====
-        if (Input.GetKey(KeyCode.Z))
-        {
-            rect.Rotate(0, 0, rotationSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.X))
-        {
-            rect.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
-        }
+        if (rotateCW)
+            rt.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
 
-        // ===== IZMĒRA MAIŅA =====
-        float deltaX = 0f;
-        float deltaY = 0f;
+        if (rotateCWW)
+            rt.Rotate(0, 0, rotationSpeed * Time.deltaTime);
 
-        // X ass (pa kreisi / pa labi)
-        if (Input.GetKey(KeyCode.RightArrow))
-            deltaX = scaleSpeed;
-        if (Input.GetKey(KeyCode.LeftArrow))
-            deltaX = -scaleSpeed;
+        if (scaleUpY && rt.localScale.y < 0.8f)
+            rt.localScale += new Vector3(0, scaleSpeed * Time.deltaTime, 0);
 
-        // Y ass (augšup / lejup)
-        if (Input.GetKey(KeyCode.UpArrow))
-            deltaY = scaleSpeed;
-        if (Input.GetKey(KeyCode.DownArrow))
-            deltaY = -scaleSpeed;
+        if (scaleDownY && rt.localScale.y > 0.8f)
+            rt.localScale += new Vector3(0, scaleSpeed * Time.deltaTime, 0);
 
-        // Piemēro izmaiņas, bet ierobežo robežās
-        float newScaleX = Mathf.Clamp(currentScale.x + deltaX, minScale, maxScale);
-        float newScaleY = Mathf.Clamp(currentScale.y + deltaY, minScale, maxScale);
+        if (scaleUpX && rt.localScale.y < 0.8f)
+            rt.localScale += new Vector3(scaleSpeed * Time.deltaTime, 0, 0);
 
-        rect.localScale = new Vector3(newScaleX, newScaleY, 1f);
+        if (scaleDownX && rt.localScale.y < 0.8f)
+            rt.localScale += new Vector3(scaleSpeed * Time.deltaTime, 0, 0);
+
+        isTransforming = rotateCW || rotateCCW || scaleUpY || scaleUpX || scaleDownX || scaleDownY;
     }
+
+    public void StartRotateCW(BaseEventData data){rotateCW = true;}
+    public void StartRotateCW(BaseEventData data){rotateCW = false;}
+
+    public void StartRotateCWW(BaseEventData data){rotateCWW = true;}
+    public void StartRotateCWW(BaseEventData data){rotateCWW = false;}
+
+    public void StartScaleUpY(BaseEventData data){scaleUpY = true;}
+    public void StartScaleUpY(BaseEventData data){scaleUpY = false;}
+    
+    public void StartScaleUpX(BaseEventData data){scaleUpX = false;}
+    public void StartScaleUpX(BaseEventData data){scaleUpX = true;}
+
+    public void StartScaleDownX(BaseEventData data){scaleDownX = true;}
+    public void StartScaleDownX(BaseEventData data){scaleDownX = false;}
+
+    public void StartScaleDownY(BaseEventData data){scaleDownY = true;}
+    public void StartScaleDownY(BaseEventData data){scaleDownY = false;}
+
 }

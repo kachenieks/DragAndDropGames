2008e181542f23c16cc07b1978c05367979cbdd4
kachenieks <katrinablazkova06@gmail.com>
2025-11-03 15:46:18 +0200

    Changes for Android 03.11.2025. - Fixed Menu Manager and DragAndDropPlace script

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,16 +7,19 @@ public class MenuManager : MonoBehaviour
     public void PlayGame1()
     {
         SceneManager.LoadScene(1);
+        Debug.Log("Button clicked!");
     }
 
     public void PlayGame2()
     {
         SceneManager.LoadScene(2);
+        Debug.Log("Button clicked!");
     }
 
     public void BackToMainMenu()
     {
         SceneManager.LoadScene(0);
+        Debug.Log("Button clicked!");
     }
 
     // Update is called once per frame
@@ -24,5 +27,9 @@ public class MenuManager : MonoBehaviour
     {
         Debug.Log("Game closed!");
         Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
